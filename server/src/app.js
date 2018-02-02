ee690f6aec512659bcb6308197d0e5dd781f4b7f
john-bauer <johnbauer12693@gmail.com>
2018-02-01 19:21:39 -0500

    basic express setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,10 @@
-console.log('hello node')
+const express = require('express')
+
+const app = express()
+
+app.listen(8080)
+
+app.use('/', (req, res) =>{
+  res.send('hello')
+})
+console.log('app running on 8080')
